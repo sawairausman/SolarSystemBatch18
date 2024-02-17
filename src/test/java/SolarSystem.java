93a9465fed560b83d0df2a982dac94bfce5940e2
sawairausman <ozsavanah@gmail.com>
2024-02-17 12:41:33 -0500

    i have worked on a planet

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -11,5 +11,10 @@ public class SolarSystem {
         xros.temp="23243243243234k";
         xros.Size="234243432432432424miles";
 
+        planet2 gecko =new planet2();
+        gecko.color="magenta";
+        gecko.temp="34343443434k";
+        gecko.size="342344342m";
+
     }
 }
---
+++ src/test/java/planet2.java
@@ -0,0 +1,6 @@
+public class planet2 {
+
+    String color;
+    String size;
+    String temp;
+}
